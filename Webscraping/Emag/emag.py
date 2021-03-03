69539b66207512952beefa492748e089763ff6a3
Andrei <frosinandrei@yahoo.com>
2021-03-03 23:15:12 +0200

    Search on emag.ro and open first 5 items on different tabs

---
+++ Webscraping/Emag/emag.py
@@ -0,0 +1,18 @@
+import requests, sys, webbrowser, bs4
+
+print('Searching...')
+res = requests.get('https://www.emag.ro/search/' + ''.join(sys.argv[1:]))
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text,'html.parser')
+linkElems = soup.select('div.card-item > div > div > div.card-section-mid > h2 > a')
+
+
+#for link in linkElems:
+#    newlink = link.get('href')
+
+numOpen = min(5,len(linkElems))
+for i in range(numOpen):
+    urlToOpen = linkElems[i].get('href')
+    print('Opening',urlToOpen)
+    webbrowser.open(urlToOpen)
